346cf62188ea33a5d809ab403f7e079db8030db8
juanmndz <juanmndz19@gmail.com>
2016-03-06 19:56:08 -0700

    Adding Cart Logic

--- cartapp/app/controllers/cart_controller.rb
+++ cartapp/app/controllers/cart_controller.rb
@@ -1,4 +1,33 @@
 class CartController < ApplicationController
-  def index
+  def add
+  	id = params[:id]
+  	# if the cart has already created, use the existing cart else create a new cart
+  	if session[:cart] then
+  		cart = session[:cart]
+  	else
+  		session[:cart] = {}
+  		cart = session[:cart]
   end
+  # if the product has already been added to the cart, increment the value else set
+  if cart[id] then
+  	cart[id] = cart[id] + 1
+  else
+  	cart[id] = 1
+  end
+  redirect to :action => :index
+end #end add method
+
+def clearCart
+	session[:cart] = nil
+	redirect_to :action => :index
+end
+def index
+	#if there is a cart, pass it to the page for display else pass an empty value
+	if session[:cart] then
+		@cart = session[:cart]
+	else
+		@cart = {}
+	end
+end
+
 end
